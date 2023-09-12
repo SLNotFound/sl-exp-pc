a939991d2cd4ae39868dc0df5551c35e3a8d8c99
sunl <1255193087@qq.com>
2023-09-12 17:39:39 +0800

    定制主题

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/utils/element-ui'
+import '@/styles/element-variables.scss'
 
 Vue.config.productionTip = false
 
