db08747a167cfe127bdac3897bb90a87d49be9e3
sunl <1255193087@qq.com>
2023-09-12 17:31:36 +0800

    引入elementui

--- src/main.js
+++ src/main.js
@@ -2,10 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-
-Vue.use(ElementUI)
+import '@/utils/element-ui'
 
 Vue.config.productionTip = false
 
